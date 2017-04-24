1da8cdb61c334abd3962b638c52684f5003efb01
Peter Fessel <peter.fessel@live.de>
2017-04-24 07:41:09 +0100

    update Stryker to v0.6

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config){
     testFramework: 'jasmine',
     testRunner: 'karma',
     karmaConfigFile: 'karma.conf.js',
-    reporter: ['progress', 'clear-text', 'dots', 'html', 'event-recorder'],
+    reporter: ['progress', 'clear-text', 'dots', /*'html'*/, 'event-recorder'],
     coverageAnalysis: 'off', // 'perTest'
     plugins: ['stryker-karma-runner', 'stryker-html-reporter']
   });
